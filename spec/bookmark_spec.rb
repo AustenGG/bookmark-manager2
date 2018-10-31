0be840befbf88948f8b176de89e42be290276190
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-10-31 11:32:11 +0000

    7 all tests passing

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,10 +4,9 @@ require 'bookmark'
    describe '.all' do
      it 'returns a list of bookmarks' do
        bookmarks = Bookmark.all
-
-       expect(bookmarks).to include('http://www.makersacademy.com')
-       expect(bookmarks).to include('http://www.destroyallsoftware.com')
-       expect(bookmarks).to include('http://www.google.com')
+       expect(bookmarks).to include "http://www.makersacademy.com"
+       expect(bookmarks).to include "http://www.destroyallsoftware.com"
+       expect(bookmarks).to include "http://www.google.com"
      end
    end
  end
