62f439f54222068c489b5dfb473d5c36728e302e
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-10-31 12:06:18 +0000

    step 10 5 passes

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -7,12 +7,11 @@ end
 
 feature 'viewing bookmarks' do
      scenario 'bookmarks are visible' do
-       connection = PG.connect(dbname: 'bookmark_manager_test2')
+       Bookmark.create(url: "http://www.makersacademy.com")
+       Bookmark.create(url: "http://www.destroyallsoftware.com")
+       Bookmark.create(url: "http://www.google.com")
 
-   # Add the test data
-       connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-       connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-       connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
        visit '/bookmarks'
 
        expect(page).to have_content 'http://www.makersacademy.com'
